2cf5aa13c749b13cc09585a64e80f68878e48980
Fery Ri <133822513+FeryRi@users.noreply.github.com>
2023-10-25 15:51:46 -0600

    si se acepta, se modifica el tablero para que tenga una forma cuadrada

--- maze.py
+++ maze.py
@@ -5,6 +5,8 @@ path = Turtle(visible=False) #tortuga invisible
 
 #matriz que le da forma al laberinto
 tiles=[
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0,
@@ -21,11 +23,13 @@ tiles=[
     0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0,
     1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]
 
 #función que se encarga de dibujar el rectángulo del tablero
 #toma como parámetro la ruta en (x,y)
-def rectangle(x,y):
+def square(x,y):
     path.up() #que no pinte
     path.goto(x,y) #manda a (x,y)
     path.down #permite que pinte
@@ -33,34 +37,28 @@ def rectangle(x,y):
 
     #ciclo para hacer el dibujo
     for count in range(4):
-        #dibujar largo
         if count%2 == 0:
              path.forward(20)
-             path.left(90)
-
-        #dibujar el ancho
-        else:
-             path.forward(16)
              path.left(90)
 
     path.end_fill() #termina llenado de dibujo
 
 #función para el retorno de puntos desplazados en el rectángulo
 def offset(point):
-        x=(floor(point.x, 20) + 200)/20
-        y=(120 - floor(point.y, 20))/20
-        index= int(x + y * 14)
-        return index
+    x=(floor(point.x, 20) + 200)/20
+    y=(180 - floor(point.y, 20))/20
+    index= int(x + y * 20)
+    return index
 
 #función para validar el punto en el tablero 
 def valid(point):
-        index=offset(point)
+    index=offset(point)
 
-        #condicional para hacer la validación
-        if tiles[index]==0:
-             return False
+    #condicional para hacer la validación
+    if tiles[index]==0:
+        return False
         
-        return point.x % 20 == 0 or point.y % 16 == 0
+    return point.x % 20 == 0 or point.y % 20 == 0
 
 #función para dibujar el laberinto usando el camino
 def maze():
@@ -74,7 +72,7 @@ def maze():
         #condicional para pintar solo el camino
         if tile > 0:
              x=(index % 20) * 20 - 200
-             y = 120 - (index // 16) * 20
-             rectangle(x,y)
+             y = 180 - (index // 20) * 20
+             square(x,y)
 
 
