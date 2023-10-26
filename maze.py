7699b55c4a6a683179843a55eecda1f50a5e82a6
Fery Ri <133822513+FeryRi@users.noreply.github.com>
2023-10-25 20:52:34 -0600

    si se acepta, se agrega lo que cruzará el laberinto con su movimiento

--- maze.py
+++ maze.py
@@ -2,6 +2,8 @@ from turtle import *
 from freegames import floor, vector
 
 path = Turtle(visible=False) #tortuga invisible
+aim = vector(5, 0)
+person = vector(180, 20)
 
 #matriz que le da forma al laberinto
 tiles=[
@@ -29,39 +31,47 @@ tiles=[
 
 #función que se encarga de dibujar el rectángulo del tablero
 #toma como parámetro la ruta en (x,y)
-def square(x,y):
+def square(x, y):
+    "Draw square using path at (x, y)."
     path.up() #que no pinte
-    path.goto(x,y) #manda a (x,y)
-    path.down #permite que pinte
-    path.begin_fill #previo al llenado del dibujo
+    path.goto(x, y) #manda a (x,y)
+    path.down() #permite que pinte
+    path.begin_fill() #previo al llenado del dibujo
 
     #ciclo para hacer el dibujo
     for count in range(4):
-        if count%2 == 0:
-             path.forward(20)
-             path.left(90)
+        path.forward(20)
+        path.left(90)
 
     path.end_fill() #termina llenado de dibujo
 
 #función para el retorno de puntos desplazados en el rectángulo
 def offset(point):
-    x=(floor(point.x, 20) + 200)/20
-    y=(180 - floor(point.y, 20))/20
-    index= int(x + y * 20)
+    "Return offset of point in tiles."
+    x = (floor(point.x, 20) + 200) / 20
+    y = (180 - floor(point.y, 20)) / 20
+    index = int(x + y * 20)
     return index
 
 #función para validar el punto en el tablero 
 def valid(point):
-    index=offset(point)
+    "Return True if point is valid in tiles."
+    index = offset(point)
 
     #condicional para hacer la validación
-    if tiles[index]==0:
+    if tiles[index] == 0:
         return False
-        
+
+    index = offset(point + 19)
+
+    if tiles[index] == 0:
+        return False
+
     return point.x % 20 == 0 or point.y % 20 == 0
 
 #función para dibujar el laberinto usando el camino
 def maze():
+    "Draw world using path."
     bgcolor('black')
     path.color('pink')
 
@@ -71,8 +81,39 @@ def maze():
 
         #condicional para pintar solo el camino
         if tile > 0:
-             x=(index % 20) * 20 - 200
-             y = 180 - (index // 20) * 20
-             square(x,y)
+            x = (index % 20) * 20 - 200
+            y = 180 - (index // 20) * 20
+            square(x, y)
 
+#función para el movimiento del objeto
+def move():
+    "Move pacman and all ghosts."
+    #para que objeto se mueva si no choca con muro
+    if valid(person + aim):
+        person.move(aim)
+
+    up()
+    goto(person.x + 10, person.y + 10)
+    dot(20, 'blue') #tamaño y color de objeto/punto
+    ontimer(move, 100) #para la velocidad del movimiento media
+
+#función para cambiar la dirección del objeto si es válido
+def change(x, y):
+    "Change pacman aim if valid."
+    if valid(person + vector(x, y)):
+        aim.x = x
+        aim.y = y
+
+
+setup(420, 340, 420, 340) #configura la ventana del juego
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: change(5, 0), 'Right')
+onkey(lambda: change(-5, 0), 'Left')
+onkey(lambda: change(0, 5), 'Up')
+onkey(lambda: change(0, -5), 'Down')
+maze()
+move()
+done()
 
